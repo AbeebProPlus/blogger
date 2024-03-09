6cf54dc6e9fcb67953cb7f9ed4744a3a388709aa
AbeebProPlus <mdahlat94@gmail.com>
2024-03-09 18:39:16 +0100

    implementing jwt

--- model/User.js
+++ model/User.js
@@ -30,7 +30,7 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    role: {
+    roles: {
         User: {type: Number, default: 2000},
         Admin: Number
     },
--- services/authenticationService.js
+++ services/authenticationService.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/User");
+const jwt = require("jsonwebtoken")
 
 const authenticate = async (req, res) => {
   const { emailOrUsername, password } = req.body;
@@ -19,10 +20,35 @@ const authenticate = async (req, res) => {
     }
     const passwordMatches = await bcrypt.compare(password, foundUser.password);
     if (passwordMatches) {
-      res.status(200).json({ message: `User with ${emailOrUsername} authenticated` });
-    } else {
-      res.sendStatus(401).json({ message: "Invalid credentials" });
-    }
+        const roles = Object.values(foundUser.roles);
+        const accessToken = jwt.sign(
+          {
+            userInfo: {
+              username: foundUser.email,
+              roles: roles,
+            },
+          },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "300s" }
+        );
+        const refreshToken = jwt.sign(
+          { username: foundUser.email },
+          process.env.REFRESH_TOKEN_SECRET,
+          { expiresIn: "1d" }
+        );
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result)
+        res.cookie("jwt", refreshToken, {
+          httpOnly: true,
+          sameSite: "None",
+          // secure: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        });
+        res.status(200).json({ accessToken });
+      } else {
+        res.sendStatus(401);
+      }
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
