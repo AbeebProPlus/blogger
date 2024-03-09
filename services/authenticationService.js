1adf3e8701196b942125dfa99b7ed8cb9cc2b7f9
AbeebProPlus <mdahlat94@gmail.com>
2024-03-09 18:14:26 +0100

    authenticate with email/username and password

--- services/authenticationService.js
+++ services/authenticationService.js
@@ -2,18 +2,24 @@ const bcrypt = require("bcrypt");
 const User = require("../model/User");
 
 const authenticate = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
+  const { emailOrUsername, password } = req.body;
+  if (!emailOrUsername || !password) {
     return res.status(400).json({ message: "Email and password are required" });
   }
   try {
-    const foundUser = await User.findOne({ email: email }).exec();
+    let foundUser;
+    if (emailOrUsername.includes("@")){
+        foundUser = await User.findOne({ "email": emailOrUsername }).exec();
+    }else{
+        foundUser = await User.findOne({"userName": emailOrUsername}).exec()
+    }
+
     if (!foundUser) {
       return res.status(403).json({ message: `User does not exist` });
     }
     const passwordMatches = await bcrypt.compare(password, foundUser.password);
     if (passwordMatches) {
-      res.status(200).json({ message: `User with ${email} authenticated` });
+      res.status(200).json({ message: `User with ${emailOrUsername} authenticated` });
     } else {
       res.sendStatus(401).json({ message: "Invalid credentials" });
     }
